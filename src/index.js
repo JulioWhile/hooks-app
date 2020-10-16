57e48d2a8c957e23393cf771bca0ecca6232a87f
Julio Conteras <julio.contreras0429@gmail.com>
2020-10-16 10:54:41 -0700

    avance en hooks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { HookApp } from './HookApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 
-ReactDOM.render(<FormWithCustomHook />, document.getElementById('root'));
+ReactDOM.render(<MultipleCustomHooks />, document.getElementById('root'));
