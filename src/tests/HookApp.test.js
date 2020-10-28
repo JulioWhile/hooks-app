0e9ab591ea2d53ab085e38e836f4d32a71367126
Julio Conteras <julio.contreras0429@gmail.com>
2020-10-28 10:58:16 -0700

    inicio en pruebas

---
+++ src/tests/HookApp.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { HookApp } from '../HookApp';
+import '@testing-library/jest-dom';
+
+describe('Pruebas en <HookApp />', () => {
+	test('Debe de mostrarse correctamente', () => {
+		const wrapper = shallow(<HookApp />);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+});
